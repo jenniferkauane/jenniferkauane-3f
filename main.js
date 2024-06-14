93a68709f98bc2c4e2d18a4be851231c8877916e
jenniferkauane <110115877+jenniferkauane@users.noreply.github.com>
2024-06-14 08:58:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const contador = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date( 2025, 4, 1);
+const tempoObjetivo2 = new Date( 2026, 2, 21);
+const tempoObjetivo3 = new Date( 2025, 11, 2);
+const tempoObjetivo4 = new Date( 2026, 0, 2);
 
+contador[0].textContent= "hello."
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
