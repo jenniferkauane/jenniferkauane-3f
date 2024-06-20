19c786aea0c0b3b0828ec27f01d1e981527659ab
jenniferkauane <110115877+jenniferkauane@users.noreply.github.com>
2024-06-20 09:41:00 -0300

    Add files via upload
    20-06

--- main.js
+++ main.js
@@ -5,8 +5,13 @@ const tempoObjetivo1 = new Date( 2025, 4, 1);
 const tempoObjetivo2 = new Date( 2026, 2, 21);
 const tempoObjetivo3 = new Date( 2025, 11, 2);
 const tempoObjetivo4 = new Date( 2026, 0, 2);
+let agora = new Date();
 
-contador[0].textContent= "hello."
+contador[0].textContent= tempoObjetivo1 - agora
+contador[1].textContent= tempoObjetivo2 - agora
+contador[2].textContent= tempoObjetivo3 - agora
+contador[3].textContent= tempoObjetivo4 - agora
+
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
