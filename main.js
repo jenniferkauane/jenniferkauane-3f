d4eb14fff255ae8f687305331e4452bad97469cc
jenniferkauane <110115877+jenniferkauane@users.noreply.github.com>
2024-06-21 08:56:46 -0300

    Add files via upload
    21-06

--- main.js
+++ main.js
@@ -6,8 +6,18 @@ const tempoObjetivo2 = new Date( 2026, 2, 21);
 const tempoObjetivo3 = new Date( 2025, 11, 2);
 const tempoObjetivo4 = new Date( 2026, 0, 2);
 let agora = new Date();
+let segundos
+let minutos
+let horas
+let dias
+ segundos = (tempoObjetivo1-agora)/1000;
+ minutos = segundos/60
+ horas = minutos/60
+ dias = horas/24
 
-contador[0].textContent= tempoObjetivo1 - agora
+
+contador[0].textContent= `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
+
 contador[1].textContent= tempoObjetivo2 - agora
 contador[2].textContent= tempoObjetivo3 - agora
 contador[3].textContent= tempoObjetivo4 - agora
