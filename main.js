b2b19e50a87b870b8c7c5ec8e9425399c9b95f21
jenniferkauane <110115877+jenniferkauane@users.noreply.github.com>
2024-09-06 08:47:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,17 +35,20 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias, horas, minutos, segundos]
     } else {
         return "Prazo Finalizado";
     }
 }
 
 function atualizaCronometro(){
-     document.getElementById("dias0") = textContent = calculaTempo(tempos[0]) 
-  //  for (let i=0; i<contadores.length;i++){
-  //      contadores[i].textContent = calculaTempo(tempos[i]);  
- //   }
+       document.getElementById('dia0').textContent = calculaTempo(tempos[0])[0]
+       document.getElementById('hora0').textContent = calculaTempo(tempos[0])[1]
+       document.getElementById('minuto0').textContent = calculaTempo(tempos[0])[2]
+       document.getElementById('segundo0').textContent = calculaTempo(tempos[0])[3]
+    for (let i=0; i<contadores.length;i++){
+        //contadores[i].textContent = calculaTempo(tempos[i]);  
+    }
 }
 
 function comecaCronometro(){
